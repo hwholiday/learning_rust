d71c379b16635c2e8df725cc56c5f6cd77f140e1
hwholiday <heweiholiday@gmail.com>
2022-03-19 11:19:53 +0800

    hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
 use learning_rust::references_and_borrowing::fn_borrowing;
 
 fn main() {
+    hashmap();
+    string();
     fn_borrowing();
-    let vec:Vec<String> =Vec::new();
     let v = vec![1, 2, 3, 4, 5];
 
     let third = &v[2];
@@ -14,6 +16,27 @@ fn main() {
 
     println!("user {:?}", user);
     println!("pwd {}", user.pwd())
+}
+
+fn hashmap(){
+    let mut conn:HashMap<String,u32>=HashMap::new();
+    conn.insert("a".to_string(),1);
+    conn.insert("b".to_string(),2);
+
+    println!("{:?}",conn);
+    conn.entry("c".to_string()).or_insert(2);
+    let conn_a=conn.entry("a".to_string()).or_insert(4);
+    *conn_a+=3;
+    println!("{:?}",conn);
+}
+
+fn string() {
+    let a = "a".to_string();
+    let b = String::from("b22222222");
+    let d =&b[0..1];
+    println!("{}",d);
+    let c = b + &a;
+    println!("{} {}", a, c)
 }
 
 #[derive(Debug)]

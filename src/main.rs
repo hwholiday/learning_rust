fca38dde70e9bf25c31d281d51b94d09b14dbe9e
hwholiday <heweiholiday@gmail.com>
2022-03-19 10:55:21 +0800

    vec

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use learning_rust::references_and_borrowing::fn_borrowing;
+
 fn main() {
-    println!("Hello, world!")
+    fn_borrowing();
+    let vec:Vec<String> =Vec::new();
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third = &v[2];
+    println!("The third element is {}", third);
+    let user = User {
+        name: String::from("1"),
+        pass: String::from("2"),
+    };
+
+    println!("user {:?}", user);
+    println!("pwd {}", user.pwd())
+}
+
+#[derive(Debug)]
+struct User {
+    name: String,
+    pass: String,
+}
+
+impl User {
+    fn pwd(&self) -> bool {
+        if self.pass == "2" && self.name == "1" {
+            return false;
+        }
+        return true;
+    }
 }

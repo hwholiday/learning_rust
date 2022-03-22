d503888ff3cef9fb3c5f10a6239db3083135d545
hwholiday <heweiholiday@gmail.com>
2022-03-22 09:59:36 +0800

    使用迭代器和闭包

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,29 +1,30 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
-use std::{env, process};
+use std::env;
 
 #[cfg(test)]
 mod tests {
+    use std::env;
+
     #[test]
-    // cargo test mini_grep  -- frog 2.txt  --show-output
     fn mini_grep() {
-        super::mini_grep()
+        let host_name = env::var("HOSTNAME").expect("HOSTNAME not definition");
+        println!("HOSTNAME {}", host_name);
     }
 }
 
 #[derive(Debug)]
-struct Config {
+pub struct Config {
     grep: String,
     name: String,
 }
 
 impl Config {
-    fn new(mut args: env::Args) -> Result<Config, &'static str> {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough args");
         }
-        args.next();
         args.next();
         let grep_str = match args.next() {
             Some(t) => t,
@@ -40,20 +41,8 @@ impl Config {
     }
 }
 
-fn mini_grep() {
-    let host_name = env::var("HOSTNAME").expect("HOSTNAME not definition");
-    println!("HOSTNAME {}", host_name);
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        println!("Config::new err {}", err);
-        process::exit(1);
-    });
-    if let Err(e) = run(config) {
-        println!("Application error: {}", e);
-        process::exit(1);
-    };
-}
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut file = File::open(&config.name)?;
     let mut content = String::new();
     file.read_to_string(&mut content)?;
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,14 @@
-use learning_rust::references_and_borrowing::fn_borrowing;
-use learning_rust::tiait::{UserLogin, UserLoginInterFace};
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::Read;
-
+use std::{env, process};
+use minigrep::{Config,run};
 fn main() {
-    let u=UserLogin{
-        name:"1".to_string(),
-        pwd:"2".to_string(),
-    };
-    println!("{}",u.verify());
-    println!("{}",u.verifyv2());
-    read_file();
-    hashmap();
-    string();
-    fn_borrowing();
-    let v = vec![1, 2, 3, 4, 5];
-
-    let third = &v[2];
-    println!("The third element is {}", third);
-    let user = User {
-        name: String::from("1"),
-        pass: String::from("2"),
-    };
-
-    println!("user {:?}", user);
-    println!("pwd {}", user.pwd())
-}
-
-
-fn read_file() {
-    let mut f = File::open("src/hello.txt").unwrap_or_else(|err|{
-        panic!("{:?}", err)
+    //cargo run -- nobody ./minigrep/2.txt
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        println!("Config::new err {}", err);
+        process::exit(1);
     });
-    let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Ok(s) => println!("len {}", s),
-        Err(e) => println!("{:?}", e),
-    }
-    println!("{}",s)
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    };
 }
 
-fn hashmap() {
-    let mut conn: HashMap<String, u32> = HashMap::new();
-    conn.insert("a".to_string(), 1);
-    conn.insert("b".to_string(), 2);
-
-    println!("{:?}", conn);
-    conn.entry("c".to_string()).or_insert(2);
-    let conn_a = conn.entry("a".to_string()).or_insert(4);
-    *conn_a += 3;
-    println!("{:?}", conn);
-}
-
-fn string() {
-    let a = "a".to_string();
-    let b = String::from("b22222222");
-    let d = &b[0..1];
-    println!("{}", d);
-    let c = b + &a;
-    println!("{} {}", a, c)
-}
-
-#[derive(Debug)]
-struct User {
-    name: String,
-    pass: String,
-}
-
-impl User {
-    fn pwd(&self) -> bool {
-        if self.pass == "2" && self.name == "1" {
-            return false;
-        }
-        return true;
-    }
-}

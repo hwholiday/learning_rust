0f7da6df78f8a2a7e224f6133e2515a9a99ffbff
hwholiday <heweiholiday@gmail.com>
2023-01-18 20:42:28 +0800

    learn status

---
+++ learnstruct/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let mut user = User::new("name".to_string(), "pwd".to_string());
+    let login_succeed = user.login_succeed();
+    println!("{}", login_succeed);
+    user.set_name("h".to_string());
+    user.set_pwd("w".to_string());
+    let login_succeed = user.login_succeed();
+    println!("{}", login_succeed);
+    user.set_name2("a".to_string());
+    println!("{:?}", user);
+    in_array(&[1,2,3])
+}
+#[derive(Debug)]
+struct User {
+    name: String,
+    pwd: String,
+}
+
+impl User {
+    fn new(name: String, pwd: String) -> User {
+        User {
+            name: String::from(name),
+            pwd: String::from(pwd),
+        }
+    }
+    fn login_succeed(&self) -> bool {
+        if self.name == "h" && self.pwd == "w" {
+            true
+        } else {
+            false
+        }
+    }
+    fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+    fn set_pwd(&mut self, pwd: String) {
+        self.pwd = pwd;
+    }
+}
+
+impl User {
+    fn set_name2(&mut self, name: String) {
+        self.name = name;
+    }
+}

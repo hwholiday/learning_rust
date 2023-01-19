e4e0a79d44cd6b115a531c0d7945fcd966effd96
hwholiday <heweiholiday@gmail.com>
2023-01-19 22:57:09 +0800

    init

--- learniterate/src/main.rs
+++ learniterate/src/main.rs
@@ -26,25 +26,23 @@ struct User {
     name: String,
 }
 
-fn f_struct(){
-   let  mut user_ver = Vec::new();
-   user_ver.push(User{
-    name:String::from("value")
-   });
-   for v in &user_ver{
-    println!("{:?}",v.name)
-   }
-
+fn f_struct() {
+    let mut user_ver = Vec::new();
+    user_ver.push(User {
+        name: String::from("value"),
+    });
+    for v in &user_ver {
+        println!("{:?}", v.name)
+    }
 }
 
-fn f_map(){
+fn f_map() {
     //let mut hash_map=HashMap::new();
-    let mut hash_map=HashMap::with_capacity(3);
+    let mut hash_map = HashMap::with_capacity(3);
     hash_map.insert("1", "333");
     hash_map.insert("2", "333");
     hash_map.insert("3", "333");
-    for (key,val) in &hash_map{
-        println!("key is {} val is {}",key,val);
+    for (key, val) in &hash_map {
+        println!("key is {} val is {}", key, val);
     }
-
 }

1d8a13f5e80cb767adc6866c8281a7f418d9c3e3
hwholiday <heweiholiday@gmail.com>
2023-02-01 15:53:07 +0800

    gateway

---
+++ gateway/src/lib.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+pub mod pkg;
+
+pub fn setup(){
+    tracing_subscriber::fmt()
+        .json()
+        .with_timer(pkg::log::LocalTimer)
+        .with_max_level(tracing::Level::TRACE)
+        .with_current_span(false)
+        .with_writer(io::stdout) // 写入标准输出
+        .init();
+
+}
--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,28 +1,11 @@
-use std::io;
+use gateway::setup;
 
-use chrono::prelude::*;
 use tracing::info;
-use tracing_subscriber::fmt::{format::Writer, time::FormatTime};
 
 fn main() {
+    setup();
     println!("gateway");
-    tracing_subscriber::fmt()
-        .json()
-        .with_timer(LocalTimer)
-        .with_max_level(tracing::Level::TRACE)
-        .with_current_span(false)
-        .with_writer(io::stdout) // 写入标准输出
-        .init();
-
     let port = 111;
     info!(port, "22"=11,"33"=22,"connected to {:?}", "222");
 }
 
-struct LocalTimer;
-
-impl FormatTime for LocalTimer {
-    fn format_time(&self, w: &mut Writer<'_>) -> std::fmt::Result {
-        //write!(w, "{}", Local::now().timestamp_millis())
-        write!(w, "{}", Local::now())
-    }
-}

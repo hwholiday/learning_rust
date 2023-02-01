d76bead8fdd18ac8d17c8151d6d37799c929c573
hwholiday <heweiholiday@gmail.com>
2023-02-01 16:05:48 +0800

    gateway

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,11 +1,23 @@
 use gateway::setup;
+use tokio::net::{TcpListener, TcpStream};
+use tracing::{error, info};
 
-use tracing::info;
-
-fn main() {
+#[tokio::main]
+async fn main() {
     setup();
-    println!("gateway");
-    let port = 111;
-    info!(port, "22"=11,"33"=22,"connected to {:?}", "222");
+    info!("gateway");
+    service().await;
 }
 
+async fn service() {
+    let listener = TcpListener::bind("0.0.0.0:8081").await.unwrap();
+    loop {
+        let (socket, socket_addr) = listener.accept().await.unwrap();
+        println!("socket_addr {:?}", socket_addr);
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {}

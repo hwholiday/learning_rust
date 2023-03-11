bf7b2c38d67e91c7d12b86f75531b99a99bc5148
hwholiday <heweiholiday@gmail.com>
2023-03-11 17:01:47 +0800

    simplechat

--- simplechat/src/lib.rs
+++ simplechat/src/lib.rs
@@ -8,6 +8,7 @@ pub fn setup(){
         .with_timer(pkg::log::LocalTimer)
         .with_max_level(tracing::Level::TRACE)
         .with_current_span(false)
+        .with_target(false)
         .with_writer(io::stdout) // 写入标准输出
         .init();
 
